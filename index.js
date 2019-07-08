c4740dbb6a525b58190c7a453554d0607589cfec
Tim Doherty <tim.doherty@procore.com>
2019-07-08 14:23:12 -0700

    implement first task in todo list

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+class WasRun {
+  constructor(name) {
+    this.wasRun = "None";
+    this.name = name;
+  }
+
+  testMethod() {
+    this.wasRun = true;
+  }
+
+  run() {
+    const method = this[this.name];
+    method();
+  }
+}
+
+const test = new WasRun("testMethod");
+console.log(test.wasRun);
+test.testMethod();
+console.log(test.wasRun);
