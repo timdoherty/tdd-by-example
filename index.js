fab051a92c50efdfbdd267780441b58258787f6d
Pierce Frawley <pierce@procore.com>
2019-09-17 14:42:09 -0700

    Add test result and test failed result

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function assert(value) {
+function assert(value, opt = '') {
+  console.log(`Assert: ${opt}`);
   if (Boolean(value) === false) {
     throw new Error("Failed to assert value");
+  }
+}
+
+class TestResult {
+  constructor() {
+    this.runCount = 0;
+  }
+
+  summary() {
+    return `${this.runCount} run, 0 failed`;
+  }
+
+  testStarted() {
+    this.runCount = this.runCount + 1;
   }
 }
 
@@ -10,10 +25,13 @@ class TestCase {
   }
 
   run() {
+    const result = new TestResult();
+    result.testStarted();
     this.setUp();
     const method = this[this.name];
     method.call(this);
     this.tearDown();
+    return result;
   }
 
   setUp() {}
@@ -32,6 +50,10 @@ class WasRun extends TestCase {
     this.log = `${this.log} testMethod`;
   }
 
+  testBrokenMethod() {
+    throw new Error("Can't read 'Feels Weird Man' of undefined");
+  }
+
   setUp() {
     this.wasRun = false;
     this.wasSetUp = true;
@@ -47,8 +69,22 @@ class TestCaseTest extends TestCase {
   testTemplateMethod() {
     this.test = new WasRun("testMethod");
     this.test.run();
-    assert("setUp testMethod tearDown" === this.test.log);
+    assert("setUp testMethod tearDown" === this.test.log, String(this.test.log));
+  }
+
+  testResult() {
+    const test = new WasRun("testMethod");
+    const result = test.run();
+    assert("1 run, 0 failed" == result.summary(), String(result.summary()));
+  }
+
+  testFailedResult() {
+    const test = new WasRun("testBrokenMethod");
+    const result = test.run();
+    assert("1 run, 1 failed" === result.summary(), String(result.summary()));
   }
 }
 
 new TestCaseTest("testTemplateMethod").run();
+new TestCaseTest("testResult").run();
+new TestCaseTest("testFailedResult").run();
